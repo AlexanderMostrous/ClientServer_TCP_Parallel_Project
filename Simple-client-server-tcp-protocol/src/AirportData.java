98bc2175bc23f44b4f1880e3f8260fcaafe512c3
alexandros <alexandros@Alexander-Desktop>
2019-04-29 17:30:22 +0300

    First commit and push

---
+++ Simple-client-server-tcp-protocol/src/AirportData.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+
+public class AirportData {
+	private static final int colNumber = 3;
+	
+	private ArrayList<String[]> rows = new ArrayList<String[]>();
+	public AirportData()
+	{
+		initializeDummy();
+	}
+	
+	
+	/*
+	 * Evresh grammhs ston pinaka me anazhthsh vasei tou kwdikou dromologiou mias pthshs.
+	 */
+	public String[] getLine(String code)
+	{
+		String[] s = new String[colNumber];
+		
+		boolean found = false;
+		int counter = 0;
+		
+		while(!found)
+		{
+			if(rows.get(counter)[0].equals(code))
+			{
+				found = true;
+				s = rows.get(counter);
+			}
+		}
+		if(found)
+			return s;
+		else
+			return null;
+	}
+	
+	
+	private void initializeDummy()
+	{
+		rows.add(new String[] {"XY4352","Arrival","12:40"});
+		rows.add(new String[] {"KY5434","Departure","02:35"});
+		rows.add(new String[] {"AG4598","Arrival","17:15"});
+		rows.add(new String[] {"BD4311","Arrival","12:00"});
+		rows.add(new String[] {"XY4389","Departure","23:30"});
+		rows.add(new String[] {"XY9761","Departure","00:55"});
+	}
+}
