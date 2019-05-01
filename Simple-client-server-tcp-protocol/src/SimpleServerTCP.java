6649e6e81a4834f4eebf641b3b942bcc05388e65
Alexander <Alexander@laptop>
2019-05-01 22:43:24 +0300

    Server ready #1 attempt.

--- Simple-client-server-tcp-protocol/src/SimpleServerTCP.java
+++ Simple-client-server-tcp-protocol/src/SimpleServerTCP.java
@@ -2,29 +2,40 @@ import java.net.*;
 import java.io.*;
 
 public class SimpleServerTCP {
+	
 	private static final int PORT = 1234;
-
+	private static AirportData ad = new AirportData();
+	
+	@SuppressWarnings("resource")
 	public static void main(String args[]) throws IOException {
 
 		ServerSocket connectionSocket = new ServerSocket(PORT);
 		System.out.println("Server is listening to port: " + PORT);	
 
+		while(true)
+		{
 		Socket dataSocket = connectionSocket.accept();
 		System.out.println("Received request from " + dataSocket.getInetAddress());
 
+		Deliverer d = new Deliverer(dataSocket, ad);
+		d.run();
+		
+		/*
 		InputStream is = dataSocket.getInputStream();
 		BufferedReader in = new BufferedReader(new InputStreamReader(is));
 		OutputStream os = dataSocket.getOutputStream();
 		PrintWriter out = new PrintWriter(os,true);
-
+		
+		
 		String inmsg, outmsg;
 		inmsg = in.readLine();
 		ServerProtocol app = new ServerProtocol();
 		outmsg = app.processRequest(inmsg);
 		out.println(outmsg);
-
-		dataSocket.close();
-		System.out.println("Data socket closed");	
+		*/
+		}
+		//dataSocket.close();
+		//System.out.println("Data socket closed");	
 	}
 }			
 
