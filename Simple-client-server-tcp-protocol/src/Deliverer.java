8237d2077bf488eda97b644ab5bb2b695b66648f
Alexander <Alexander@laptop>
2019-05-01 15:36:04 +0300

    Modifications on Deliverer class.

--- Simple-client-server-tcp-protocol/src/Deliverer.java
+++ Simple-client-server-tcp-protocol/src/Deliverer.java
@@ -1,16 +1,36 @@
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Deliverer implements Runnable{
 
 	private Socket socket;
+	private AirportData data;
 	
-	public Deliverer(Socket aSocket){
+	public Deliverer(Socket aSocket, AirportData ad){
 		this.socket = aSocket;
+		this.data = ad;
 	}
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Connected: " + socket);
+		String clientCommand = "";
+        try {
+        	Scanner in = new Scanner(socket.getInputStream());
+        	PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            while (in.hasNextLine()) {
+                clientCommand = in.nextLine();
+            }
+            
+            if(clientCommand.startsWith("READ"))
+            	;
+        } catch (Exception e) {
+            System.out.println("Error:" + socket);
+        } finally {
+            try { socket.close(); } catch (IOException e) {}
+            System.out.println("Closed: " + socket);
+        }
 	}
 	
 	
