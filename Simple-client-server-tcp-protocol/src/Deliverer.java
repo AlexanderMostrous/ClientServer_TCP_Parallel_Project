f58a217ec851659370c4d4587d1c75c18504ca9a
Alexander <Alexander@laptop>
2019-05-01 15:04:01 +0300

    Deliverer class creation.

---
+++ Simple-client-server-tcp-protocol/src/Deliverer.java
@@ -0,0 +1,18 @@
+import java.net.Socket;
+
+public class Deliverer implements Runnable{
+
+	private Socket socket;
+	
+	public Deliverer(Socket aSocket){
+		this.socket = aSocket;
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
